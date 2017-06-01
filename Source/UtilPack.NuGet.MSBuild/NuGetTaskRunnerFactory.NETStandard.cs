7a1c4e7552d72664bdb5d48cc1eb44460dfb3721
Stanislav Muhametsin <stanislav.muhametsin@cometasolutions.fi>
2017-06-01 13:32:25 +0300

    The .NETStandard version of UtilPack.NuGet.MSBuild project works, but requires MSBuild 15.3 (preview), since Task Factories are not supported at all in current MSBuild 15.1 for .NET Core.

--- Source/UtilPack.NuGet.MSBuild/NuGetTaskRunnerFactory.NETStandard.cs
+++ Source/UtilPack.NuGet.MSBuild/NuGetTaskRunnerFactory.NETStandard.cs
@@ -20,7 +20,10 @@ using NuGet.Frameworks;
 using NuGet.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.Collections.Concurrent;
 
 namespace UtilPack.NuGet.MSBuild
 {
@@ -34,7 +37,117 @@ namespace UtilPack.NuGet.MSBuild
          IDictionary<String, ISet<String>> assemblyPathsBySimpleName
          )
       {
-         throw new NotImplementedException();
+         // .NETStandard 1.5+ uses System.Runtime.Loader.AssemblyContext instead of app domains.
+         return new NETStandardExecutionHelper( assemblyPathsBySimpleName, new NuGetResolverWrapper( nugetResolver ), assemblyPath, taskName, taskFactoryLoggingHost );
+      }
+
+      // We must subclass System.Runtime.Loader.AssemblyLoadContext, since the default load context does not seem to launch Resolve event for System.* assemblies (and thus we never catch load request for e.g. System.Threading.Tasks.Extensions assembly).
+      private sealed class NuGetTaskLoadContext : System.Runtime.Loader.AssemblyLoadContext
+      {
+         private readonly CommonAssemblyRelatedHelper _helper;
+
+         private readonly ConcurrentDictionary<AssemblyName, Lazy<Assembly>> _loadedAssemblies;
+
+         public NuGetTaskLoadContext( CommonAssemblyRelatedHelper helper )
+         {
+            this._helper = helper;
+            this._loadedAssemblies = new ConcurrentDictionary<AssemblyName, Lazy<Assembly>>(
+               ComparerFromFunctions.NewEqualityComparer<AssemblyName>(
+                  ( x, y ) => String.Equals( x.Name, y.Name ) && String.Equals( x.CultureName, y.CultureName ) && x.Version.Equals( y.Version ) && ArrayEqualityComparer<Byte>.ArrayEquality( x.GetPublicKeyToken(), y.GetPublicKeyToken() ),
+                  x => x.Name.GetHashCode()
+                  )
+               );
+         }
+
+         protected override Assembly Load( AssemblyName assemblyName )
+         {
+            // Some System.* packages are provided by .NET Core runtime, and some via NuGet extensions
+            // Unfortunately, the only way to distinguish them (in order to avoid loading assembly duplicates from NuGet packages) is to try using default context *first*, and then use our own custom context in case the default context fails to load the assembly.
+            // There is no other way to see failed assembly load than catching exception ( LoadFromAssemblyName never returns null ).
+            // A pity that API was designed like that.
+
+            // To save amount of catches we do, cache all loaded assembly by their assembly names.
+            return this._loadedAssemblies.GetOrAdd( assemblyName, an => new Lazy<Assembly>( () =>
+            {
+               Assembly retVal = null;
+               try
+               {
+                  retVal = Default.LoadFromAssemblyName( assemblyName );
+               }
+               catch
+               {
+                  // Ignore
+               }
+               return retVal ?? this._helper.PerformAssemblyResolve( an );
+            } ) ).Value;
+         }
+      }
+
+      private sealed class NETStandardExecutionHelper : CommonAssemblyRelatedHelper, NuGetTaskExecutionHelper
+      {
+         private readonly System.Runtime.Loader.AssemblyLoadContext _loader;
+         private readonly Lazy<Type> _taskType;
+         private Microsoft.Build.Framework.IBuildEngine _be;
+
+         public NETStandardExecutionHelper(
+            IDictionary<String, ISet<String>> assemblyPathsBySimpleName,
+            NuGetResolverWrapper resolver,
+            String targetAssemblyPath,
+            String taskName,
+            Microsoft.Build.Framework.IBuildEngine taskFactoryBE
+            )
+            : base( assemblyPathsBySimpleName, resolver, targetAssemblyPath, taskName )
+         {
+            this._loader = new NuGetTaskLoadContext( this );
+            this._be = taskFactoryBE;
+            // Register to default load context resolving event to minimize amount of exceptions thrown.
+            System.Runtime.Loader.AssemblyLoadContext.Default.Resolving += this._loader_Resolving;
+            this._taskType = new Lazy<Type>( () => this.LoadTaskType() );
+         }
+
+         private Assembly _loader_Resolving( System.Runtime.Loader.AssemblyLoadContext loader, AssemblyName name )
+         {
+            return this.PerformAssemblyResolve( name );
+         }
+
+         public Object CreateTaskInstance( Type taskType, Microsoft.Build.Framework.IBuildEngine taskLoggingHost )
+         {
+            // The taskLoggingHost is the BE given to the task.
+            this._be = taskLoggingHost;
+            return this.PerformCreateTaskInstance( taskType );
+         }
+
+         public void Dispose()
+         {
+            // TODO maybe some day will be possible to say this._loader.UnloadAll()...
+            System.Runtime.Loader.AssemblyLoadContext.Default.Resolving -= this._loader_Resolving;
+         }
+
+         public Microsoft.Build.Framework.TaskPropertyInfo[] GetTaskParameters()
+         {
+            return CommonHelpers.GetPropertyInfoFromType(
+               this._taskType.Value,
+               typeof( Microsoft.Build.Framework.ITask ).GetTypeInfo().Assembly.GetName()
+               )
+               .Where( kvp => kvp.Value.Item1 != WrappedPropertyKind.BuildEngine && kvp.Value.Item1 != WrappedPropertyKind.TaskHost )
+               .Select( kvp => new Microsoft.Build.Framework.TaskPropertyInfo( kvp.Key, kvp.Value.Item3.PropertyType, kvp.Value.Item2 == WrappedPropertyInfo.Out, kvp.Value.Item2 == WrappedPropertyInfo.Required ) )
+               .ToArray();
+         }
+
+         public Type GetTaskType()
+         {
+            return this._taskType.Value;
+         }
+
+         protected override void LogResolveMessage( String message )
+         {
+            this._be.LogMessageEvent( new Microsoft.Build.Framework.BuildMessageEventArgs( message, null, null, Microsoft.Build.Framework.MessageImportance.High ) );
+         }
+
+         protected override Assembly LoadAssemblyFromPath( String path )
+         {
+            return this._loader.LoadFromAssemblyPath( path );
+         }
       }
    }
 }
--- Source/UtilPack.NuGet.MSBuild/NuGetTaskRunnerFactory.cs
+++ Source/UtilPack.NuGet.MSBuild/NuGetTaskRunnerFactory.cs
@@ -27,6 +27,10 @@ using NuGet.Versioning;
 using System.Reflection;
 using NuGet.Frameworks;
 
+using TPropertyInfo = System.ValueTuple<UtilPack.NuGet.MSBuild.WrappedPropertyKind, UtilPack.NuGet.MSBuild.WrappedPropertyInfo, System.Reflection.PropertyInfo>;
+using TNuGetPackageResolverCallback = System.Func<System.String, System.String, System.String[], System.Boolean, System.String, System.Reflection.Assembly>;
+using TAssemblyByPathResolverCallback = System.Func<System.String, System.Reflection.Assembly>;
+
 namespace UtilPack.NuGet.MSBuild
 {
    public partial class NuGetTaskRunnerFactory : ITaskFactory
@@ -38,6 +42,11 @@ namespace UtilPack.NuGet.MSBuild
 
       private NuGetTaskExecutionHelper _helper;
       private Type _taskType;
+
+      public NuGetTaskRunnerFactory()
+      {
+
+      }
 
       public String FactoryName => nameof( NuGetTaskRunnerFactory );
 
@@ -93,7 +102,7 @@ namespace UtilPack.NuGet.MSBuild
          var retVal = false;
          if ( resolveInfo != null && resolveInfo.TryGetValue( package, out var assemblyPaths ) )
          {
-            var assemblyPath = NuGetBoundResolver.GetAssemblyPathFromNuGetAssemblies( assemblyPaths, package.ExpandedPath, taskBodyElement.Element( "AssemblyPath" )?.Value );
+            var assemblyPath = CommonHelpers.GetAssemblyPathFromNuGetAssemblies( assemblyPaths, package.ExpandedPath, taskBodyElement.Element( "AssemblyPath" )?.Value );
 
             if ( !String.IsNullOrEmpty( assemblyPath ) )
             {
@@ -287,8 +296,16 @@ namespace UtilPack.NuGet.MSBuild
          return Path.Combine( NuGetEnvironment.GetFolderPath( NuGetFolderPath.NuGetHome ), "packages" );
       }
 
-      // This method is in this class since this class has no implemented interfaces and extends System.Object.
-      // Therefore using this static method from another appdomain won't cause any assembly resolves.
+
+   }
+
+   // These methods are used by both .net45 and .netstandard.
+   // This class has no implemented interfaces and extends System.Object.
+   // Therefore using this static method from another appdomain won't cause any assembly resolves.
+   internal static class CommonHelpers
+   {
+      internal const String MBF = "Microsoft.Build.Framework.";
+
       public static String GetAssemblyPathFromNuGetAssemblies(
          String[] assemblyPaths,
          String packageExpandedPath,
@@ -310,7 +327,131 @@ namespace UtilPack.NuGet.MSBuild
          }
          return assemblyPath;
       }
+
+      public static IDictionary<String, TPropertyInfo> GetPropertyInfoFromType(
+         Type type,
+         AssemblyName msbuildFrameworkAssemblyName
+         )
+      {
+         // Doing typeof( Microsoft.Build.Framework.ITask ).Assembly.GetName().FullName; will cause MSBuild 14.0 assembly to be loaded in net45 build, if target assembly is .netstandard assembly.
+         // This most likely due the fact that net45 build requires msbuild 14.X (msbuild 15.X requires net46).
+         // So, just get the msbuildTaskAssemblyName from original appdomain as a parameter to this method.
+         // That is why MBF string consts & other helper constructs exist, and why we can't cast stuff directly to Microsoft.Build.Framework types.
+
+
+         var retVal = new Dictionary<String, TPropertyInfo>();
+         foreach ( var property in type.GetRuntimeProperties().Where( p => ( p.GetMethod?.IsPublic ?? false ) && ( p.SetMethod?.IsPublic ?? false ) ) )
+         {
+            var curProperty = property;
+            var propertyType = curProperty.PropertyType;
+            var actualType = propertyType;
+            if ( actualType.IsArray )
+            {
+               actualType = actualType.GetElementType();
+            }
+            WrappedPropertyKind? kind;
+            switch ( Type.GetTypeCode( actualType ) )
+            {
+               case TypeCode.Object:
+                  if ( ISMFBType( actualType, msbuildFrameworkAssemblyName ) )
+                  {
+                     if ( Equals( actualType.FullName, MBF + nameof( Microsoft.Build.Framework.IBuildEngine ) ) )
+                     {
+                        kind = WrappedPropertyKind.BuildEngine;
+                     }
+                     else if ( Equals( actualType.FullName, MBF + nameof( Microsoft.Build.Framework.ITaskHost ) ) )
+                     {
+                        kind = WrappedPropertyKind.TaskHost;
+                     }
+                     else if ( Equals( actualType.FullName, MBF + nameof( Microsoft.Build.Framework.ITaskItem ) ) )
+                     {
+                        kind = WrappedPropertyKind.TaskItem;
+                     }
+                     else if ( Equals( actualType.FullName, MBF + nameof( Microsoft.Build.Framework.ITaskItem2 ) ) )
+                     {
+                        kind = WrappedPropertyKind.TaskItem2;
+                     }
+                     else
+                     {
+                        kind = null;
+                     }
+                  }
+                  else
+                  {
+                     kind = null;
+                  }
+                  break;
+#if !NETSTANDARD1_5
+               case TypeCode.DBNull:
+#endif
+               case TypeCode.Empty:
+                  kind = null;
+                  break;
+               case TypeCode.String:
+                  kind = WrappedPropertyKind.StringNoConversion;
+                  break;
+               default:
+                  kind = WrappedPropertyKind.String;
+                  break;
+            }
+
+            if ( kind.HasValue )
+            {
+               WrappedPropertyInfo info;
+               var customMBFAttrs = curProperty.GetCustomAttributes( true )
+                  .Where( ca => ISMFBType( ca.GetType(), msbuildFrameworkAssemblyName ) )
+                  .ToArray();
+               if ( customMBFAttrs.Any( ca => Equals( ca.GetType().FullName, MBF + nameof( Microsoft.Build.Framework.RequiredAttribute ) ) ) )
+               {
+                  info = WrappedPropertyInfo.Required;
+               }
+               else if ( customMBFAttrs.Any( ca => Equals( ca.GetType().FullName, MBF + nameof( Microsoft.Build.Framework.OutputAttribute ) ) ) )
+               {
+                  info = WrappedPropertyInfo.Out;
+               }
+               else
+               {
+                  info = WrappedPropertyInfo.None;
+               }
+
+               retVal.Add( curProperty.Name, (kind.Value, info, curProperty) );
+            }
+         }
+
+         return retVal;
+      }
+
+      private static Boolean ISMFBType( Type type, AssemblyName mfbAssembly )
+      {
+         var an = type
+#if NETSTANDARD1_5
+                     .GetTypeInfo()
+#endif
+                     .Assembly.GetName();
+         Byte[] pk;
+         return String.Equals( an.Name, mfbAssembly.Name )
+            && ( pk = an.GetPublicKeyToken() ) != null
+            && mfbAssembly.GetPublicKeyToken().SequenceEqual( pk );
+      }
    }
+
+   internal enum WrappedPropertyKind
+   {
+      String,
+      StringNoConversion,
+      TaskItem,
+      TaskItem2,
+      BuildEngine,
+      TaskHost
+   }
+
+   internal enum WrappedPropertyInfo
+   {
+      None,
+      Required,
+      Out
+   }
+
 
    internal interface NuGetTaskExecutionHelper : IDisposable
    {
@@ -319,5 +460,255 @@ namespace UtilPack.NuGet.MSBuild
       TaskPropertyInfo[] GetTaskParameters();
 
       Object CreateTaskInstance( Type taskType, IBuildEngine taskFactoryLoggingHost );
+   }
+
+   internal abstract class CommonAssemblyRelatedHelper
+   {
+      private readonly IDictionary<String, IDictionary<String, Lazy<Assembly>>> _assemblyPathsBySimpleName; // We will get multiple requests to load same assembly, so cache them
+      //private readonly String _thisAssemblyName;
+      private readonly NuGetResolverWrapper _resolver;
+      private readonly String _targetAssemblyPath;
+      private readonly String _taskName;
+
+      protected CommonAssemblyRelatedHelper(
+         IDictionary<String, ISet<String>> assemblyPathsBySimpleName,
+         NuGetResolverWrapper resolver,
+         String targetAssemblyPath,
+         String taskName
+         )
+      {
+         this._assemblyPathsBySimpleName = assemblyPathsBySimpleName.ToDictionary(
+            kvp => kvp.Key,
+            kvp => (IDictionary<String, Lazy<Assembly>>) kvp.Value.ToDictionary( fullPath => fullPath, fullPath => new Lazy<Assembly>( () => this.LoadAssemblyFromPath( fullPath ) ) )
+         );
+         this._resolver = resolver;
+         this._targetAssemblyPath = targetAssemblyPath;
+         this._taskName = taskName;
+         //         this._thisAssemblyName = this.GetType()
+         //#if NETSTANDARD1_5
+         //            .GetTypeInfo()
+         //#endif
+         //            .Assembly.FullName;
+      }
+
+      internal protected Assembly PerformAssemblyResolve( AssemblyName assemblyName )
+      {
+         Assembly retVal;
+         //         if ( String.Equals( this._thisAssemblyName, assemblyName.FullName ) )
+         //         {
+         //            // Happens when calling Initialize method (because the original appdomain has this assembly loaded "normally", but this app-domain has this assembly loaded via file path because of CreateInstanceFrom method.)
+         //            retVal = this.GetType()
+         //#if NETSTANDARD1_5
+         //            .GetTypeInfo()
+         //#endif
+         //               .Assembly;
+         //         }
+         //         else 
+         if ( this._assemblyPathsBySimpleName.TryGetValue( assemblyName.Name, out var assemblyLazies ) )
+         {
+            retVal = assemblyLazies.FirstOrDefault( kvp =>
+            {
+               var defName = kvp.Value.IsValueCreated ? kvp.Value.Value.GetName() :
+#if NETSTANDARD1_5
+               System.Runtime.Loader.AssemblyLoadContext
+#else
+               AssemblyName
+#endif
+               .GetAssemblyName( kvp.Key );
+
+               return AssemblyNamesMatch( assemblyName, defName );
+            } ).Value?.Value;
+
+            this.LogResolveMessage( retVal == null ?
+               $"Assembly reference did not match definition for \"{assemblyName}\", considered \"{String.Join( ";", assemblyLazies.Keys )}\"." :
+               $"Found \"{assemblyName}\" by simple name \"{assemblyName.Name}\" in \"{retVal.CodeBase}\"." );
+         }
+         else
+         {
+            this.LogResolveMessage( $"Failed to find \"{assemblyName}\" by simple name \"{assemblyName.Name}\"." );
+            retVal = null;
+         }
+         return retVal;
+      }
+
+      private static Boolean AssemblyNamesMatch(
+         AssemblyName reference,
+         AssemblyName definition
+         )
+      {
+         String refStr; String defStr;
+         if ( reference.Flags.HasFlag( AssemblyNameFlags.Retargetable ) )
+         {
+            refStr = reference.Name;
+            defStr = definition.Name;
+         }
+         else
+         {
+            refStr = reference.FullName;
+            defStr = reference.FullName;
+         }
+
+         return String.Equals( refStr, defStr );
+      }
+
+      internal protected Type LoadTaskType()
+      {
+         var taskAssembly = this.PerformAssemblyResolve(
+#if NETSTANDARD1_5
+            System.Runtime.Loader.AssemblyLoadContext.GetAssemblyName
+#else
+            AssemblyName.GetAssemblyName
+#endif
+            ( this._targetAssemblyPath )
+            );
+
+         return taskAssembly.GetType( this._taskName, false, false );
+      }
+
+      internal protected Object PerformCreateTaskInstance(
+         Type type
+         )
+      {
+         var ctors = type
+#if NETSTANDARD1_5
+            .GetTypeInfo()
+#endif
+            .GetConstructors();
+         ConstructorInfo matchingCtor = null;
+         Object[] ctorParams = null;
+         if ( ctors.Length > 0 )
+         {
+            var ctorInfo = new Dictionary<Int32, IDictionary<ISet<Type>, ConstructorInfo>>();
+
+            foreach ( var ctor in ctors )
+            {
+               var paramz = ctor.GetParameters();
+               ctorInfo
+                  .GetOrAdd_NotThreadSafe( paramz.Length, pl => new Dictionary<ISet<Type>, ConstructorInfo>( SetEqualityComparer<Type>.DefaultEqualityComparer ) )
+                  .Add( new HashSet<Type>( paramz.Select( p => p.ParameterType ) ), ctor );
+            }
+
+            if (
+               ctorInfo.TryGetValue( 2, out var curInfo )
+               && curInfo.TryGetValue( new HashSet<Type>() { typeof( TNuGetPackageResolverCallback ), typeof( TAssemblyByPathResolverCallback ) }, out matchingCtor )
+               )
+            {
+               ctorParams = new Object[2];
+               ctorParams[Array.FindIndex( matchingCtor.GetParameters(), p => p.ParameterType.Equals( typeof( TNuGetPackageResolverCallback ) ) )] = (TNuGetPackageResolverCallback) this.LoadNuGetAssembly;
+               ctorParams[Array.FindIndex( matchingCtor.GetParameters(), p => p.ParameterType.Equals( typeof( TAssemblyByPathResolverCallback ) ) )] = (TAssemblyByPathResolverCallback) this.LoadOtherAssembly;
+            }
+            else if ( ctorInfo.TryGetValue( 1, out curInfo ) )
+            {
+               if ( curInfo.TryGetValue( new HashSet<Type>( typeof( TNuGetPackageResolverCallback ).Singleton() ), out matchingCtor ) )
+               {
+                  ctorParams = new Object[] { (TNuGetPackageResolverCallback) this.LoadNuGetAssembly };
+               }
+               else if ( curInfo.TryGetValue( new HashSet<Type>( typeof( TAssemblyByPathResolverCallback ).Singleton() ), out matchingCtor ) )
+               {
+                  ctorParams = new Object[] { (TAssemblyByPathResolverCallback) this.LoadOtherAssembly };
+               }
+            }
+            else if ( ctorInfo.TryGetValue( 0, out curInfo ) )
+            {
+               matchingCtor = curInfo.Values.First();
+            }
+         }
+
+         if ( matchingCtor == null )
+         {
+            throw new Exception( $"No public suitable constructors found for type {type.AssemblyQualifiedName}." );
+         }
+
+         return matchingCtor.Invoke( ctorParams );
+      }
+
+      // This method can get called by target task to dynamically load nuget assemblies.
+      private Assembly LoadNuGetAssembly(
+         String packageID,
+         String packageVersion,
+         String[] repositories,
+         Boolean loadDependencies,
+         String assemblyPath
+         )
+      {
+         var assemblyInfos = this._resolver.ResolveNuGetPackageAssemblies( packageID, packageVersion, repositories, loadDependencies, out var packageKey, out var packagePath );
+         Assembly retVal = null;
+         if ( assemblyInfos != null )
+         {
+            var assembliesBySimpleName = this._assemblyPathsBySimpleName;
+            foreach ( var kvp in assemblyInfos )
+            {
+               foreach ( var nugetAssemblyPath in kvp.Value )
+               {
+                  var curPath = nugetAssemblyPath;
+                  assembliesBySimpleName
+                     .GetOrAdd_NotThreadSafe( Path.GetFileNameWithoutExtension( curPath ), sn => new Dictionary<String, Lazy<Assembly>>() )
+                     .GetOrAdd_NotThreadSafe( curPath, cp => new Lazy<Assembly>( () => this.LoadAssemblyFromPath( cp ) ) );
+               }
+            }
+
+            var possibleAssemblyPaths = assemblyInfos[packageKey];
+            assemblyPath = CommonHelpers.GetAssemblyPathFromNuGetAssemblies( possibleAssemblyPaths, packagePath, assemblyPath );
+            if ( !String.IsNullOrEmpty( assemblyPath ) )
+            {
+               retVal = assembliesBySimpleName
+                  [Path.GetFileNameWithoutExtension( assemblyPath )]
+                  [assemblyPath]
+                  .Value;
+            }
+         }
+
+         return retVal;
+      }
+
+      // This method can get called by target task to dynamically load assemblies by path.
+      private Assembly LoadOtherAssembly(
+         String assemblyPath
+         )
+      {
+         assemblyPath = Path.GetFullPath( assemblyPath );
+         Assembly retVal = null;
+         if ( File.Exists( assemblyPath ) )
+         {
+            retVal = this._assemblyPathsBySimpleName
+               .GetOrAdd_NotThreadSafe( Path.GetFileNameWithoutExtension( assemblyPath ), ap => new Dictionary<String, Lazy<Assembly>>() )
+               .GetOrAdd_NotThreadSafe( assemblyPath, ap => new Lazy<Assembly>( () => this.LoadAssemblyFromPath( ap ) ) )
+               .Value;
+         }
+         return retVal;
+      }
+
+      protected abstract void LogResolveMessage( String message );
+
+      protected abstract Assembly LoadAssemblyFromPath( String path );
+   }
+
+   // Instances of this class reside in task factory app domain.
+   internal sealed class NuGetResolverWrapper
+#if !NETSTANDARD1_5
+      : MarshalByRefObject
+#endif
+   {
+      private readonly NuGetBoundResolver _resolver;
+
+      public NuGetResolverWrapper( NuGetBoundResolver resolver )
+      {
+         this._resolver = resolver;
+      }
+
+      public IDictionary<String, String[]> ResolveNuGetPackageAssemblies(
+         String packageID,
+         String packageVersion,
+         String[] repositoryPaths,
+         Boolean loadDependencies,
+         out String givenPackageString,
+         out String packageExpandedPath
+         )
+      {
+         var retVal = this._resolver.ResolveNuGetPackages( packageID, packageVersion, repositoryPaths, loadDependencies, out var package );
+         givenPackageString = package?.ToString();
+         packageExpandedPath = package?.ExpandedPath;
+         return retVal.ToDictionary( kvp => kvp.Key.ToString(), kvp => kvp.Value );
+      }
    }
 }

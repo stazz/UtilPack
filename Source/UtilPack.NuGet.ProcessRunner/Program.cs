7f7454b4a11edcda44e41b4e3fb44617a2ee18cb
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2018-03-21 00:20:08 +0200

    Added a feature to UtilPack.NuGet.ProcessRunner which enables it to be used to only deploy the application without executing it.

--- Source/UtilPack.NuGet.ProcessRunner/Program.cs
+++ Source/UtilPack.NuGet.ProcessRunner/Program.cs
@@ -87,36 +87,38 @@ namespace UtilPack.NuGet.ProcessRunner
 
                if ( !String.IsNullOrEmpty( assemblyPath ) && framework != null )
                {
-
-                  if ( framework != null && String.IsNullOrEmpty( monitorConfig.ToolPath ) )
+                  if ( !programConfig.DeployOnly )
                   {
-                     monitorConfig.ToolPath = TryAutoDetectTool( framework );
-                  }
-
-                  Console.Out.Write( $"\n\nInitialization is complete, starting process located in {assemblyPath}.\n\n" );
-
-                  // Monitor step - start process, and keep running until it exits or this process exits.
-                  // Restart the target process if it requests it.
-                  const String PROCESS_ARG_PREFIX = "/ProcessArgument:";
-                  // We don't know how target process parses arguments, and we don't want to make assumptions
-                  // The Microsoft.Extensions.Configuration will only see one argument with the example arguments:
-                  // /ProcessArgument:MyArg=34 /ProcessArgument:MyArg:Test=35
-                  // So we need to parse these ourselves
-                  var monitoring = new ProcessMonitor.ProcessMonitor(
-                     monitorConfig,
-                     args.Where( arg => arg.StartsWith( PROCESS_ARG_PREFIX ) )
-                     .Select( arg => arg.Substring( PROCESS_ARG_PREFIX.Length ) )
-                     );
-                  errorsSeen = await monitoring.KeepMonitoringAsync(
-                     assemblyPath,
-                     source.Token
-                     );
-                  if ( errorsSeen && programConfig.PauseBeforeExitIfErrorsSeen )
-                  {
-                     await Console.Out.WriteLineAsync( "Monitored process showed errors, press any key to terminate this process." );
-                     while ( !Console.KeyAvailable )
+                     if ( framework != null && String.IsNullOrEmpty( monitorConfig.ToolPath ) )
                      {
-                        await Task.Delay( 100 );
+                        monitorConfig.ToolPath = TryAutoDetectTool( framework );
+                     }
+
+                     Console.Out.Write( $"\n\nInitialization is complete, starting process located in {assemblyPath}.\n\n" );
+
+                     // Monitor step - start process, and keep running until it exits or this process exits.
+                     // Restart the target process if it requests it.
+                     const String PROCESS_ARG_PREFIX = "/ProcessArgument:";
+                     // We don't know how target process parses arguments, and we don't want to make assumptions
+                     // The Microsoft.Extensions.Configuration will only see one argument with the example arguments:
+                     // /ProcessArgument:MyArg=34 /ProcessArgument:MyArg:Test=35
+                     // So we need to parse these ourselves
+                     var monitoring = new ProcessMonitor.ProcessMonitor(
+                        monitorConfig,
+                        args.Where( arg => arg.StartsWith( PROCESS_ARG_PREFIX ) )
+                        .Select( arg => arg.Substring( PROCESS_ARG_PREFIX.Length ) )
+                        );
+                     errorsSeen = await monitoring.KeepMonitoringAsync(
+                        assemblyPath,
+                        source.Token
+                        );
+                     if ( errorsSeen && programConfig.PauseBeforeExitIfErrorsSeen )
+                     {
+                        await Console.Out.WriteLineAsync( "Monitored process showed errors, press any key to terminate this process." );
+                        while ( !Console.KeyAvailable )
+                        {
+                           await Task.Delay( 100 );
+                        }
                      }
                   }
                   retVal = 0;
@@ -176,5 +178,7 @@ namespace UtilPack.NuGet.ProcessRunner
       public String NuGetConfigurationFile { get; set; }
 
       public Boolean PauseBeforeExitIfErrorsSeen { get; set; }
+
+      public Boolean DeployOnly { get; set; }
    }
 }

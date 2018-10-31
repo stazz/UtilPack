68142e3a2e6d461fc7bbe3b387df7a741d1455f1
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-10-31 23:24:09 +0200

    Renaming UtilPack.NuGet.Deployment -> NuGetUtils.Lib.Deployment. Still need to fit that into tool program.

--- Source/NuGetUtils.Lib.Tool/Configuration.cs
+++ Source/NuGetUtils.Lib.Tool/Configuration.cs
@@ -22,66 +22,167 @@ using System;
 
 namespace NuGetUtils.Lib.Tool
 {
+   /// <summary>
+   /// This is data interface for configuration which deals with using <see cref="BoundRestoreCommandUser"/> via <see cref="NuGetRestoringProgram{TCommandLineConfiguration, TConfigurationConfiguration}"/>.
+   /// </summary>
+   /// <remarks>All properties are optional.</remarks>
    public interface NuGetUsageConfiguration
    {
+      /// <summary>
+      /// Gets the path to NuGet configuration file.
+      /// </summary>
+      /// <value>The path to NuGet configuration file.</value>
       String NuGetConfigurationFile { get; }
 
-
+      /// <summary>
+      /// Gets the name of the NuGet framework that restoring is performed against.
+      /// </summary>
+      /// <value>The name of the NuGet framework that restoring is performed against.</value>
       String RestoreFramework { get; }
 
+      /// <summary>
+      /// Gets the lock file cache directory path.
+      /// </summary>
+      /// <value>The lock file cache directory path.</value>
       String LockFileCacheDirectory { get; }
 
-
+      /// <summary>
+      /// Gets the SDK NuGet package ID.
+      /// </summary>
+      /// <value>The SDK NuGet package ID.</value>
       String SDKFrameworkPackageID { get; }
 
-
+      /// <summary>
+      /// Gets the SDK NuGet package version.
+      /// </summary>
+      /// <value>The SDK NuGet package version.</value>
       String SDKFrameworkPackageVersion { get; }
 
-
+      /// <summary>
+      /// Gets the value indicating whether to disable caching <see cref="LockFile"/>s to disk.
+      /// </summary>
+      /// <value>The value indicating whether to disable caching <see cref="LockFile"/>s to disk.</value>
       Boolean DisableLockFileCache { get; }
 
-
+      /// <summary>
+      /// Gets the <see cref="NuGet.Common.LogLevel"/> for NuGet logger.
+      /// </summary>
+      /// <value>the <see cref="NuGet.Common.LogLevel"/> for NuGet logger.</value>
       LogLevel LogLevel { get; }
 
-
+      /// <summary>
+      /// Gets the value indicating whether to disable logging altogether.
+      /// </summary>
+      /// <value>The value indicating whether to disable logging altogether.</value>
       Boolean DisableLogging { get; }
    }
 
+   /// <summary>
+   /// This is data interface for configuration which describes the JSON file where the actual configuration should be read from, typically using <see cref="Program{TCommandLineConfiguration, TConfigurationConfiguration}"/>.
+   /// </summary>
    public interface ConfigurationConfiguration
    {
+      /// <summary>
+      /// Gets the path to the JSON file containing the actual configuration.
+      /// </summary>
+      /// <value>The path to the JSON file containing the actual configuration.</value>
       String ConfigurationFileLocation { get; }
    }
 
+   /// <summary>
+   /// This class contains string constants that can be used in <see cref="UtilPack.Documentation.DescriptionAttribute"/> for properties of types implementing <see cref="NuGetUsageConfiguration"/> and <see cref="ConfigurationConfiguration"/>.
+   /// </summary>
    public static class DefaultDocumentation
    {
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="NuGetUsageConfiguration.NuGetConfigurationFile"/>.
+      /// </summary>
       public const String NuGetConfigurationFileValue = "path";
+
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.NuGetConfigurationFile"/>.
+      /// </summary>
       public const String NuGetConfigurationFileDescription = "The path to NuGet configuration file containing the settings to use. The default NuGet setting file behaviour is the default if this is not specified.";
 
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="NuGetUsageConfiguration.RestoreFramework"/>.
+      /// </summary>
       public const String RestoreFrameworkValue = "framework";
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.RestoreFramework"/>.
+      /// </summary>
       public const String RestoreFrameworkDescription = "The name of the current framework of the process (e.g. \".NETCoreApp,v=2.1\"). If automatic detection of the process framework does not work, then use this parameter to override.";
 
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="NuGetUsageConfiguration.LockFileCacheDirectory"/>.
+      /// </summary>
       public const String LockFileCacheDirectoryValue = "path";
 
+      /// <summary>
+      /// The first part of the <see cref="LockFileCacheDirectoryDescription"/>, containing the string before the <see cref="NuGetRestoringProgramConsts.LOCK_FILE_CACHE_DIR_ENV_NAME"/>.
+      /// </summary>
       public const String LockFileCacheDirectoryDescription1 = "The path of the directory acting as cache directory for restored package information (serialized " + nameof( LockFile ) + "s). By default, the environment variable \"";
-      public const String LockFileCacheDirectoryDescription2 = "\" is first checked, and if that is not present, the \"";
-      public const String LockFileCacheDirectoryDescription3 = "\" directory within current home directory is used.";
-      public const String LockFileCacheDirectoryDescription = LockFileCacheDirectoryDescription1 + Consts.LOCK_FILE_CACHE_DIR_ENV_NAME + LockFileCacheDirectoryDescription2 + Consts.LOCK_FILE_CACHE_DIR_WITHIN_HOME_DIR + LockFileCacheDirectoryDescription3;
 
+      /// <summary>
+      /// The second part of the <see cref="LockFileCacheDirectoryDescription"/>, containing the string after the <see cref="NuGetRestoringProgramConsts.LOCK_FILE_CACHE_DIR_ENV_NAME"/> and before the <see cref="NuGetRestoringProgramConsts.LOCK_FILE_CACHE_DIR_WITHIN_HOME_DIR"/>.
+      /// </summary>
+      public const String LockFileCacheDirectoryDescription2 = "\" is first checked, and if that is not present, the \"";
+      /// <summary>
+      /// The third part of the <see cref="LockFileCacheDirectoryDescription"/>, containing the string after the <see cref="NuGetRestoringProgramConsts.LOCK_FILE_CACHE_DIR_WITHIN_HOME_DIR"/>.
+      /// </summary>
+      public const String LockFileCacheDirectoryDescription3 = "\" directory within current home directory is used.";
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.LockFileCacheDirectory"/>.
+      /// </summary>
+      public const String LockFileCacheDirectoryDescription = LockFileCacheDirectoryDescription1 + NuGetRestoringProgramConsts.LOCK_FILE_CACHE_DIR_ENV_NAME + LockFileCacheDirectoryDescription2 + NuGetRestoringProgramConsts.LOCK_FILE_CACHE_DIR_WITHIN_HOME_DIR + LockFileCacheDirectoryDescription3;
+
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="NuGetUsageConfiguration.SDKFrameworkPackageID"/>.
+      /// </summary>
       public const String SDKFrameworkPackageIDValue = "packageID";
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.SDKFrameworkPackageID"/>.
+      /// </summary>
       public const String SDKFrameworkPackageIDDescription = "The package ID of the current framework SDK package. This depends on a auto-detected or explicitly specified restore framework, but is typically \"" + NuGetUtility.SDK_PACKAGE_NETCORE + "\".";
 
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="NuGetUsageConfiguration.SDKFrameworkPackageVersion"/>.
+      /// </summary>
       public const String SDKFrameworkPackageVersionValue = "version";
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.SDKFrameworkPackageVersion"/>.
+      /// </summary>
       public const String SDKFrameworkPackageVersionDescription = "The package version of the framework SDK package. By default, it is deduced from path of the assembly containing " + nameof( Object ) + " type.";
+
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.DisableLockFileCache"/>.
+      /// </summary>
 
       public const String DisableLockFileCacheDescription = "Whether to disable using cache directory for restored package information (serialized " + nameof( LockFile ) + "s).";
 
-
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="NuGetUsageConfiguration.LogLevel"/>.
+      /// </summary>
       public const String LogLevelValue = "level";
+
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.LogLevel"/>.
+      /// </summary>
       public const String LogLevelDescription = "Which log level to use for NuGet logger. By default, this is " + nameof( LogLevel.Information ) + ".";
 
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="NuGetUsageConfiguration.DisableLogging"/>.
+      /// </summary>
       public const String DisableLoggingDescription = "Whether to disable NuGet logging completely.";
 
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.ValueName"/> for property <see cref="ConfigurationConfiguration.ConfigurationFileLocation"/>.
+      /// </summary>
       public const String ConfigurationFileLocationValue = "path";
+
+      /// <summary>
+      /// The value for <see cref="UtilPack.Documentation.DescriptionAttribute.Description"/> for property <see cref="ConfigurationConfiguration.ConfigurationFileLocation"/>.
+      /// </summary>
       public const String ConfigurationFileLocationDescription = "The path to the JSON file containing the configuration with all the same information as normal command line parameters.";
    }
 }

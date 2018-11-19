832d084a4998e6d84b5725ceaffc3a85beb4c1e2
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-11-19 23:18:59 +0200

    Trying to fix access errors on AV build.

--- CI/build.sh
+++ CI/build.sh
@@ -11,6 +11,9 @@ GIT_ROOT=$(readlink -f "${SCRIPTDIR}/..")
 cp "${SCRIPTDIR}/CISupport.props" "${GIT_ROOT}/CISupport.props"
 cp "${SCRIPTDIR}/NuGet.config" "${GIT_ROOT}/NuGet.Config.ci"
 
+# Create key
+# echo "${ASSEMBLY_SIGN_KEY}" | base64 -d > "${SCRIPTDIR}/Keys/UtilPack.snk"
+
 # Build within docker
 docker run \
   --rm \
@@ -18,6 +21,7 @@ docker run \
   -v "${CS_OUTPUT}:/repo-dir/BuildTarget/:rw" \
   -v "${GIT_ROOT}/NuGet.config.ci:/root/.nuget/NuGet/NuGet.Config:ro" \
   -v "/nuget_package_dir/:/root/.nuget/packages/:rw" \
+  -u 0 \
   microsoft/dotnet:2.1-sdk-alpine \
   dotnet \
   build \

9bf6eaa7281498176975d8f02516d90f147d81ce
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-11-19 23:28:22 +0200

    Case-change, part2.

--- CI/build.sh
+++ CI/build.sh
@@ -9,7 +9,7 @@ GIT_ROOT=$(readlink -f "${SCRIPTDIR}/..")
 
 # Copy required files to GIT root
 cp "${SCRIPTDIR}/CISupport.props" "${GIT_ROOT}/CISupport.props"
-cp "${SCRIPTDIR}/NuGet.config" "${GIT_ROOT}/NuGet.Config.ci"
+cp "${SCRIPTDIR}/NuGet.Config" "${GIT_ROOT}/NuGet.Config.ci"
 
 # Create key
 # echo "${ASSEMBLY_SIGN_KEY}" | base64 -d > "${SCRIPTDIR}/Keys/UtilPack.snk"
@@ -19,15 +19,14 @@ docker run \
   --rm \
   -v "${GIT_ROOT}/:/repo-dir/contents/:ro" \
   -v "${CS_OUTPUT}:/repo-dir/BuildTarget/:rw" \
-  -v "${GIT_ROOT}/NuGet.config.ci:/root/.nuget/NuGet/NuGet.Config:ro" \
+  -v "${GIT_ROOT}/NuGet.Config.ci:/root/.nuget/NuGet/NuGet.Config:ro" \
   -v "/nuget_package_dir/:/root/.nuget/packages/:rw" \
   -u 0 \
   microsoft/dotnet:2.1-sdk-alpine \
-  ls -al /root/.nuget/NuGet/NuGet.Config
-  # dotnet \
-  #build \
-  #'/p:Configuration=Release' \
-  #'/p:IsCIBuild=true' \
-  #'/t:Build;Pack' \
-  #'/repo-dir/contents/Source/UtilPack.Logging'
+  dotnet \
+  build \
+  '/p:Configuration=Release' \
+  '/p:IsCIBuild=true' \
+  '/t:Build;Pack' \
+  '/repo-dir/contents/Source/UtilPack.Logging'
 

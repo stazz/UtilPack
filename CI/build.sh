2a9fb2859321c16f1907ba0ae19ef2131998f674
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-11-19 22:45:46 +0200

    More script-like CI pipeline (less dependant on which provider/environment: AppVeyor/Jenkins/etc).

---
+++ CI/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Find out the path and directory where this script resides
+SCRIPTPATH=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPTPATH")
+GIT_ROOT=$(readlink -f "${SCRIPTPATH}/.." )
+
+# Copy required files to GIT root
+cp "${SCRIPTDIR}/CISupport.props" "${GIT_ROOT}/CISupport.props"
+cp "${SCRIPTDIR}/NuGet.config" "${GIT_ROOT}/NuGet.Config.ci"
+
+# Build within docker
+docker run \
+  --rm \
+  -v "${GIT_ROOT}/:/repo-dir/contents/:ro" \
+  -v "/output/:/repo-dir/BuildTarget/:rw" \
+  -v "${GIT_ROOT}/NuGet.config.ci:/root/.nuget/NuGet/NuGet.Config:ro" \
+  -v "/nuget_package_dir/:/root/.nuget/packages/:rw" \
+  microsoft/dotnet:2.1-sdk-alpine \
+  dotnet \
+  build \
+  '/p:Configuration=Release' \
+  '/p:IsCIBuild=true' \
+  '/t:Build;Pack' \
+  '/repo-dir/contents/Source/UtilPack.Logging'
+

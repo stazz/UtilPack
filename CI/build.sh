96ef96908fde18c0c83fa6edbe07e93cafca370a
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-11-21 19:19:35 +0200

    Running custom script within the container.

--- CI/build.sh
+++ CI/build.sh
@@ -13,9 +13,9 @@ cp "${SCRIPTDIR}/CISupport.props" "${GIT_ROOT}/CISupport.props"
 cp "${SCRIPTDIR}/NuGet.Config" "${GIT_ROOT}/NuGet.Config.ci"
 
 # Create key
+mkdir -p "${GIT_ROOT}/Keys"
 set -v
 set +x
-mkdir "${GIT_ROOT}/Keys"
 echo "${ASSEMBLY_SIGN_KEY}" | base64 -d > "${GIT_ROOT}/Keys/UtilPack.snk"
 set +v
 set -x
@@ -28,9 +28,16 @@ if [[ "${RELATIVE_CS_OUTPUT}" ]]; then
   CS_OUTPUT=$(readlink -f "${BASE_ROOT}/${RELATIVE_CS_OUTPUT}")
 fi
 
+BUILD_COMMAND="dotnet build /p:Configuration=Release /p:IsCIBuild=true /t:Build;Pack /repo-dir/contents/Source/UtilPack.Logging"
+
+if [[ "${BUILD_SCRIPT_WITHIN_CONTAINER}" ]]; then
+  # Our actual build command is to invoke a script within GIT repository, and passing it the build command as parameter
+  BUILD_COMMAND="/repo-dir/contents/${BUILD_SCRIPT_WITHIN_CONTAINER} ${BUILD_COMMAND}"
+fi
+
 # find "${GIT_ROOT}/Source" -maxdepth 2 -type f -name '*.csproj' -printf '/repo-dir/contents/Source/%P '
 # Build within docker
-docker run \
+"/c/Program Files/Docker/Docker/resources/bin/docker.exe" run \
   --rm \
   -v "${GIT_ROOT}/:/repo-dir/contents/:ro" \
   -v "${CS_OUTPUT}/:/repo-dir/BuildTarget/:rw" \
@@ -38,10 +45,5 @@ docker run \
   -v "${NUGET_PACKAGE_DIR}/:/root/.nuget/packages/:rw" \
   -u 0 \
   microsoft/dotnet:2.1-sdk-alpine \
-  dotnet \
-  build \
-  '/p:Configuration=Release' \
-  '/p:IsCIBuild=true' \
-  '/t:Build;Pack' \
-  '/repo-dir/contents/Source/UtilPack.Logging'
+  ${BUILD_COMMAND}
 

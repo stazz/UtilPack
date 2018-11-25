7c3b97e0b541bdbdf8e7e026113967e77ac12adb
Stanislav Muhametsin <stanislav.muhametsin@kapsi.fi>
2018-11-25 23:11:05 +0200

    Ignore error code for mv command as it always gives warning in this case.

--- Build/download-net40-ref-assemblies.sh
+++ Build/download-net40-ref-assemblies.sh
@@ -7,5 +7,7 @@ if [[ ! -d "$1" ]]; then
   curl https://codeload.github.com/mono/reference-assemblies/zip/master --output "${TEMP_DIR_DL}/repo.zip"
   7za x -y "-o${TEMP_DIR_DL}" "${TEMP_DIR_DL}/repo.zip" reference-assemblies-master/v4.0
   mkdir "$1"
+  set +e
   mv ${TEMP_DIR_DL}/reference-assemblies-master/v4.0/{.,}* "$1"
+  set -e
 fi

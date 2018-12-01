34c69bd9d26226d2b1018e30b1d89d3851b52e66
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2018-12-01 20:48:10 +0200

    Work on proper build, test, and deploy scripts. IL fiddling is now done in deployment stage, as it should.

--- CI/test.sh
+++ CI/test.sh
@@ -20,12 +20,13 @@ if [[ "${RELATIVE_CS_OUTPUT}" ]]; then
   CS_OUTPUT=$(readlink -f "${BASE_ROOT}/${RELATIVE_CS_OUTPUT}")
 fi
 
-# TODO what to put as log filename since {} will just be full path instead of e.g. basename...
-TEST_COMMAND="find /repo-dir/contents/Source/Tests -mindepth 2 -maxdepth 2 -type f -name *.csproj -exec dotnet test -c Release --no-build --logger trx;LogFileName=/repo-dir/BuildTarget/TestResults/UtilPack.trx /p:IsCIBuild=true {} ;"
+# Run tests with hard-coded trx format, for now.
+TEST_COMMAND=(find /repo-dir/contents/Source/Tests -mindepth 2 -maxdepth 2 -type f -name *.csproj -exec sh -c 'dotnet test -c Release --no-build --logger trx\;LogFileName=/repo-dir/BuildTarget/TestResults/$(basename {} .csproj).trx /p:IsCIBuild=true {}' \;)
+
 
 if [[ "${TEST_SCRIPT_WITHIN_CONTAINER}" ]]; then
-  # Our actual build command is to invoke a script within GIT repository, and passing it the build command as parameter
-  TEST_COMMAND="/repo-dir/contents/${TEST_SCRIPT_WITHIN_CONTAINER} ${TEST_COMMAND}"
+  # Our actual command is to invoke a script within GIT repository, and passing it the command as parameter
+  TEST_COMMAND=("/repo-dir/contents/${TEST_SCRIPT_WITHIN_CONTAINER}" "${TEST_COMMAND[@]}")
 fi
 
 if [[ "${ADDITIONAL_VOLUME_DIRECTORIES}" ]]; then
@@ -41,7 +42,7 @@ if [[ "${ADDITIONAL_VOLUME_DIRECTORIES}" ]]; then
   done
 fi
 
-# Build code within docker
+# Run tests code within docker
 docker run \
   --rm \
   -v "${GIT_ROOT}/:/repo-dir/contents/:ro" \
@@ -52,13 +53,25 @@ docker run \
   -e THIS_TFM=netcoreapp2.1 \
   ${ADDITIONAL_VOLUMES_STRING} \
   microsoft/dotnet:2.1-sdk-alpine \
-  ${TEST_COMMAND}
-  
-# Because find does not return non-0 exit code even when its -exec command does, we need to make sure that we have equal amount of test reports...? as there are projects
-# SOURCE_PROJECT_COUNT=$(find "${GIT_ROOT}/Source/Tests" -mindepth 2 -maxdepth 2 -type f -name *.csproj | wc -l)
-# SOURCE_ARTIFACT_COUNT=$(find "${CS_OUTPUT}/Release/bin" -mindepth 2 -maxdepth 2 -type f -name *.nupkg | wc -l)
+  "${TEST_COMMAND[@]}"
 
-# if [[ ${SOURCE_PROJECT_COUNT} -ne ${SOURCE_ARTIFACT_COUNT} ]]; then
-#  echo "One or more project did not build successfully." 1>&2
-#  exit 1
-# fi
+# Verify that all test projects produced test report
+TEST_PROJECT_COUNT=$(find "${GIT_ROOT}/Source/Tests" -mindepth 2 -maxdepth 2 -type f -name *.csproj | wc -l)
+TEST_REPORT_COUNT=$(find "${CS_OUTPUT}/TestResults" -mindepth 1 -maxdepth 1 -type f -name *.trx | wc -l)
+
+if [[ ${TEST_PROJECT_COUNT} -ne ${TEST_REPORT_COUNT} ]]; then
+ echo "One or more project did not produce test report successfully." 1>&2
+ exit 1
+fi
+  
+# Verify that all tests in all test reports are passed.
+# Enumerate all .trx files, for each of those execute Python one-liner to get amount of executed and passed tests, print them out and save to array.
+# Each array element is two numbers separated by space character - first is executed count, second is passed count.
+readarray -t TEST_RESULTS < <(find "${CS_OUTPUT}/TestResults" -name *.trx -exec python3 -c "from xml.etree.ElementTree import ElementTree; doc = ElementTree(file='{}'); docNS = doc.getroot().tag.split('}')[0].strip('{'); counters = doc.find('test_ns:ResultSummary/test_ns:Counters', { 'test_ns': docNS }); print(counters.attrib['executed'] + ' ' + counters.attrib['passed']);" \;)
+# Walk each array element and make sure that first number matches second
+for TEST_RESULT in "${TEST_RESULTS[@]}"; do
+  EXECUTED_AND_PASSED=(${TEST_RESULT})
+  if [[ "${EXECUTED_AND_PASSED[0]}" -ne "${EXECUTED_AND_PASSED[1]}" ]]; then
+    exit 1
+  fi
+done

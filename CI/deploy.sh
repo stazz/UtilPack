7a06b9d6b029ced38fbdc32fb81eb8ef96001df1
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2018-12-02 23:23:56 +0200

    Working on deploy script some more.

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -12,15 +12,16 @@ if [[ "${RELATIVE_CS_OUTPUT}" ]]; then
 fi
 
 # Get current GIT branch
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+CURRENT_BRANCH=$(git -C "${GIT_ROOT}" rev-parse --abbrev-ref HEAD)
 if [[ "${CURRENT_BRANCH}" == "master" ]]; then
   # Get all tags for this commit
-  readarray -t CURRENT_TAGS < <(git tag --points-at)
+  readarray -t CURRENT_TAGS < <(git -C "${GIT_ROOT}" tag --points-at)
   
   if [[ "${#CURRENT_TAGS[@]}" -gt "0" ]]; then
     # Tags were defined for this commit, all tags must match the filename of the .nupkg exactly in order to get pushed.
-    # Artifacts should have been collected to CI server by this point, so now we can just copy the packages that should be pushed to a temp directory, and push all at once with one command
-    PUSH_DIR=$(mktemp -d)
+    # Artifacts should have been collected to CI server by this point, so now we can just copy the packages that should be pushed to a directory, and push all at once with one command
+    PUSH_DIR="${BASE_ROOT}/push-source"
+    mkdir "${PUSH_DIR}"
     for CURRENT_TAG in "${CURRENT_TAGS[@]}"; do
       # This will return non-zero, failing whole script, if file does not exist
       cp "${CS_OUTPUT}/Release/bin/${CURRENT_TAG}.nupkg" "${PUSH_DIR}/"
@@ -28,25 +29,30 @@ if [[ "${CURRENT_BRANCH}" == "master" ]]; then
 
     ADDITIONAL_DOCKER_ARGS=()
     if [[ "${DEPLOY_NUGET_CONFIG_FILE}" ]]; then
-      ADDITIONAL_DOCKER_ARGS+=('-v', "${GIT_ROOT}/${DEPLOY_NUGET_CONFIG_FILE}:/push-dir/NuGet.Config:ro")
+      ADDITIONAL_DOCKER_ARGS+=('-v' "${GIT_ROOT}/${DEPLOY_NUGET_CONFIG_FILE}:/push-dir/NuGet.Config:ro")
     fi
 
     ADDITIONAL_PUSH_ARGS=()
     if [[ "${DEPLOY_NUGET_SOURCE}" ]]; then
-      ADDITIONAL_PUSH_ARGS+=('--source', "${DEPLOY_NUGET_SOURCE}")
+      ADDITIONAL_PUSH_ARGS+=('--source' "${DEPLOY_NUGET_SOURCE}")
     fi
     if [[ "${DEPLOY_NUGET_SYMBOL_SOURCE}" ]]; then
-      ADDITIONAL_PUSH_ARGS+=('--symbol-source', "${DEPLOY_NUGET_SYMBOL_SOURCE}")
+      ADDITIONAL_PUSH_ARGS+=('--symbol-source' "${DEPLOY_NUGET_SYMBOL_SOURCE}")
     fi
 
+    if [[ "${DEPLOY_NUGET_NO_SERVICE_ENDPOINT}" ]]; then
+      ADDITIONAL_PUSH_ARGS+=('--no-service-endpoint')
+    fi
+
+    # Turn off expanding variables as we are dealing with secret values.
     set -v
     set +x
 
     if [[ "${DEPLOY_NUGET_API_KEY}" ]]; then
-      ADDITIONAL_PUSH_ARGS+=('--api-key', "${DEPLOY_NUGET_API_KEY}")
+      ADDITIONAL_PUSH_ARGS+=('--api-key' "${DEPLOY_NUGET_API_KEY}")
     fi
     if [[ "${DEPLOY_NUGET_SYMBOL_API_KEY}" ]]; then
-      ADDITIONAL_PUSH_ARGS+=('--symbol-api-key', "${DEPLOY_NUGET_SYMBOL_API_KEY}")
+      ADDITIONAL_PUSH_ARGS+=('--symbol-api-key' "${DEPLOY_NUGET_SYMBOL_API_KEY}")
     fi
 
     # All .nupkg files have been copied, invoke the command via docker
@@ -54,7 +60,7 @@ if [[ "${CURRENT_BRANCH}" == "master" ]]; then
       -v "${PUSH_DIR}/:/push-dir/content/:ro" \
       "${ADDITIONAL_DOCKER_ARGS[@]}" \
       -u 0 \
-      -w "/push-dir/content/" \
+      -w "/push-dir/content" \
       "microsoft/dotnet:${DOTNET_VERSION}-sdk-alpine" \
       dotnet nuget push \
       '*.nupkg' \

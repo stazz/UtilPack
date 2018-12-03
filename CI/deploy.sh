9595adec16f80e96e309f8a28e8a3f655f1029fc
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2018-12-03 20:07:36 +0200

    Typo.

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -17,7 +17,7 @@ fi
 # git rev-list --first-parent <branchname> <- this shows all commit hashes for a specific branch, therefore allows us to reliably determine whether we are on some specific branch, tip or not.
 # Don't fail on error (e.g. if master branch does not exist)
 GIT_COMMIT_HASH=$(git -C "${GIT_ROOT}" show-ref --hash HEAD)
-COMMIT_HASH_IN_MASTER=$(git -C "${GIT_ROOT}" rev-list --first-parent master -- 2>/dev/null | echo '' | grep "${GIT_COMMIT_HASH}")
+COMMIT_HASH_IN_MASTER=$(git -C "${GIT_ROOT}" rev-list --first-parent master -- 2>/dev/null || echo '' | grep "${GIT_COMMIT_HASH}")
 if [[ "${GIT_COMMIT_HASH}" == "${COMMIT_HASH_IN_MASTER}" ]]; then
   # Get all tags for this commit
   readarray -t CURRENT_TAGS < <(git -C "${GIT_ROOT}" tag --points-at)
